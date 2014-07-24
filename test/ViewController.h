55b9a39fa2a8ee1bc98a060e08fd4e3e550a7d41
Joao <joaoricardoduarte@gmail.com>
2014-07-24 11:09:22 +0100

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Joao Duarte on 24/07/2014.
+//  Copyright (c) 2014 Joao Duarte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Joao Duarte on 24/07/2014.
+//  Copyright (c) 2014 Joao Duarte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
