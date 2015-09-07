cfa278f1326478b486d6a386dd836026695eaab1
Joao <joaoricardoduarte@gmail.com>
2015-09-07 17:49:49 +0100

    testing new build again

--- test/ViewController.m
+++ test/ViewController.m
@@ -29,10 +29,6 @@
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:viewtest attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0f]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:viewtest attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0f]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:viewtest attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0f]];
-  
-  
-  [self.view lord];
-  dsads
 }
 
 @end
