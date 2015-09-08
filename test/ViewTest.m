95e8460992adc10a786e3b90a4f6b05183c56f72
Joao <joaoricardoduarte@gmail.com>
2015-09-08 09:27:00 +0100

    adding the tests target

--- test/ViewTest.m
+++ test/ViewTest.m
@@ -30,8 +30,6 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     self.firstLabel.text = @"For your added security, you will need to register your device before you can use the app. So to check that it is you, we’ll call you on a number you have previously registered with us.\n\nIf you have more than one number registered, you’ll be able to choose which number you wish to receive the call on.\n\nIf you haven’t registered a number, or you need to update your existing number(s), please log on to our desktop site and update them via the personal details section. For security purposes, you may not be able to use your new number(s) immediately.";
-  
-  fdfd.test()
 }
 
 @end
